d417b2a98d4c4a98eb28e656af87f982fabe53a0
Dmitry Klionsky <dm.klionsky@gmail.com>
2012-08-08 22:27:42 +0300

    Refine define/speak selected text

--- data/Scripts/Define selected text.py
+++ data/Scripts/Define selected text.py
@@ -1,2 +1,5 @@
 text = clipboard.get_selection()
+text = clipboard.get_selection()
 system.exec_command("gnome-open http://dictionary.reference.com/browse/" + text, False)
+command = "dict-mp3 '" + text + "'"
+system.exec_command(command, False)
--- data/Scripts/Speak selected text.py
+++ data/Scripts/Speak selected text.py
@@ -1,3 +1,7 @@
-text = clipboard.get_selection()
-system.exec_command("espeak '" + text + "'",False)
+word = clipboard.get_selection()
+command = "dict-mp3 '" + word + "'"
+try:
+    system.exec_command(command, False)
+except subprocess.CalledProcessError, e:
+    dialog.info_dialog(title = "Error", message = str(e.output))
 
